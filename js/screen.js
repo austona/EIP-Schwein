98aaf5fe8bcbc2329b7f8d1eccc9294783e8ec08
Anton <benzanton@live.com>
2017-06-12 14:59:29 +0200

    Changes in home
    Added the Ablaufschema and the Karte

--- js/screen.js
+++ js/screen.js
@@ -97,6 +97,49 @@
 	}).next().hide(); 
 		});
 		
+		//##########################################
+	// Modal for Ablaufschema
+	//##########################################
+		// Get the modal
+var modal = document.getElementById('ablaufschemaModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('ablaufschema');
+var modalImg = document.getElementById("imgAblaufschema");
+var captionText = document.getElementById("captionAblaufschema");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
+		
+var modal = document.getElementById('karteModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('karte');
+var modalImg = document.getElementById("imgKarte");
+var captionText = document.getElementById("captionKarte");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
 
 
   
